0331d71bc90d723612f31226cd2956f714e0a9c2
Christina Greene <christina.greene@rogers.com>
2019-06-16 16:37:11 -0400

    added descriptions for results and updated responsive layout

--- js/scripts.js
+++ js/scripts.js
@@ -32,61 +32,71 @@ $(document).ready(function(){
       ////////////////// RESULTS //////////////////
       // Wine Result 1 - CARBERNET SAUVIGNON
       if (totalSum <= 4) {
-        const resultsContent = `<div class="red-result">
-        <div class="results-overlay">
-        <div class="results-heading">
-        <h3>You should try a dry ${cabSauv}</h3>
-        <button type="reset" class="reset" href="#q-1" aria-label="Click to start over.">Try Again!</button>
-        </div>
-        </div>
+        const resultsContent = `
+        <div class="red-result">
+          <div class="results-overlay">
+            <div class="results-heading">
+              <h3>You should try a dry ${cabSauv}</h3>
+              <p>Cabernet Sauvignon is the most popular type of red wine around the world. It is full-bodied and tends to be on the dry end of the spectrum. This type of wine has heavy notes of red and black fruits and pairs well with aged cheese, braised beef, roasted lamb, or a good old fashioned burger.</p>
+              <button type="reset" class="reset" href="#q-1" aria-label="Click to start over.">Try Again!</button>
+            </div>
+          </div>
         </div>`
         $(`.results`).html(resultsContent);
       }
       // Wine Result 2 - MERLOT
       if (totalSum === 5) {
-        const resultsContent = `<div class="red-result">
-        <div class="results-overlay">
-        <div class="results-heading">
-        <h3>It's all about a smooth ${merlot}</h3>
-        <button type="reset" class="reset" href="#q-1" aria-label="Click to start over.">Try Again!</button>
-        </div>
-        </div>
+        const resultsContent = `
+        <div class="red-result">
+          <div class="results-overlay">
+            <div class="results-heading">
+              <h3>It's all about a smooth ${merlot}</h3>
+              <p>Why not go for a soft, velvety and fruity wine like a merlot? Depending on where the wine is cultivated, its flavour notes can vary from tabacco to chocolate. Try out a glass of merlot with lightly-spiced dark meat, herb roasted chicken, shepherd's pie or a chocolate dessert of your choosing.</p>
+              <button type="reset" class="reset" href="#q-1" aria-label="Click to start over.">Try Again!</button>
+            </div>
+          </div>
         </div>`
         $(`.results`).html(resultsContent);
       }
       // Wine Result 3 - ROSE
       if (totalSum === 6) {
-        const resultsContent = `<div class="red-result">
-        <div class="results-overlay">
-        <div class="results-heading">
-        <h3>${rose}, all day</h3>
-        <button type="reset" class="reset" href="#q-1" aria-label="Click to start over.">Try Again!</button>
-        </div>
-        </div>
+        const resultsContent = `
+        <div class="red-result">
+          <div class="results-overlay">
+            <div class="results-heading">
+              <h3>${rose}, all day</h3>
+              <p>Whether you want to enjoy it chilled or as a frozé, this fruity wine features notes of strawberry, melon, peach and rose. Rosé can be acidic and dry or fruity and full-bodied. It pairs well with almost anything from seafood to pasta to soft cheeses.</p>
+              <button type="reset" class="reset" href="#q-1" aria-label="Click to start over.">Try Again!</button>
+            </div>
+          </div>
         </div>`
         $(`.results`).html(resultsContent);
       }
       // Wine Result 4 - SAUVIGNON BLANC
       if (totalSum === 7) {
-        const resultsContent = `<div class="white-result">
-        <div class="results-overlay">
-        <div class="results-heading">
-        <h3>A crisp, fruity ${sauvignon} would hit the spot</h3>
-        <button type="reset" class="reset" href="#q-1" aria-label="Click to start over.">Try Again!</button>
-        </div>
-        </div>
+        const resultsContent = `
+        <div class="white-result">
+          <div class="results-overlay">
+            <div class="results-heading">
+              <h3>A crisp, fruity ${sauvignon} would hit the spot</h3>
+              <p>This refreshing white wine features both fruity and herbaceous notes like lime, passion fruit, jalapeño and bell pepper while staying on the dry side of the spectrum. It pairs well with herb-topped foods like poultry, white fish, sautéed vegetables and goat cheese.</p>
+              <button type="reset" class="reset" href="#q-1" aria-label="Click to start over.">Try Again!</button>
+            </div>
+          </div>
         </div>`
         $(`.results`).html(resultsContent);
       }
       // Wine Result 5 - PINOT GRIGIO
       if (totalSum >= 8) {
-        const resultsContent = `<div class="white-result">
-        <div class="results-overlay">
-        <div class="results-heading">
-        <h3>Today calls for a zesty ${pinot}</h3>
-        <button type="reset" class="reset" href="#q-1" aria-label="Click to start over.">Try Again!</button>
-        </div>
-        </div>
+        const resultsContent = `
+        <div class="white-result">
+          <div class="results-overlay">
+            <div class="results-heading">
+              <h3>Today calls for a zesty ${pinot}</h3>
+              <p>This refreshing white wine is full of citrus notes. It has hints of honeysuckle and can take on fruity flavours like apple, pear and nectarine. This dry wine pairs well with fresh vegetables, raw fish and lighter meals.</p>
+              <button type="reset" class="reset" href="#q-1" aria-label="Click to start over.">Try Again!</button>
+            </div>
+          </div>
         </div>`
         $(`.results`).html(resultsContent);
       }
